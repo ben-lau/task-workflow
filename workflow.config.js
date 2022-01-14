f8ddb80cf470fa9a10057fb285a27219fbb13e20
ben-lau <408736066@qq.com>
2022-01-15 00:12:48 +0800

    chore: 优化工程流程

--- workflow.config.js
+++ workflow.config.js
@@ -1,9 +1,5 @@
 const { Workflow, Tasks } = require('./lib/index');
 
-const On = '1';
-
-const isPatchVersion = () => process.env.patch !== On;
-
 new Workflow('to-self', {
   description: '提交到远程',
   steps: [
@@ -15,38 +11,135 @@ new Workflow('to-self', {
     },
     { name: '获取提交信息', use: Tasks.AskFor.commitMessage() },
     { name: '提交', use: Tasks.Git.commit(message => [{ message }]) },
+    { name: '推送', use: Tasks.Git.push() },
+  ],
+});
+
+new Workflow('release:major', {
+  description: 'major版本',
+  steps: [
     {
-      name: '版本信息',
+      name: '构建',
       skip: async () =>
-        !(await Tasks.AskFor.shouldContinue({
-          message: '是否更新版本？',
-        })()),
-      use: () => (process.env.patch = On),
+        !(await Tasks.AskFor.shouldContinue({ message: '是否需要构建？' })()),
+      use: Tasks.Shell.run({ cmd: 'npm run build' }),
     },
+    { name: '获取提交信息', use: Tasks.AskFor.commitMessage() },
+    { name: '提交', use: Tasks.Git.commit(message => [{ message }]) },
     {
       name: '打版本',
-      skip: isPatchVersion,
-      use: Tasks.Shell.run({ cmd: 'npm run version:patch' }),
+      use: Tasks.Shell.run({
+        cmd: 'npm run version:major',
+      }),
+    },
+    {
+      name: '生成日志',
+      use: Tasks.Shell.run({
+        cmd: 'npm run changelog',
+      }),
+    },
+    {
+      name: '提交',
+      use: Tasks.Git.commit({
+        message: 'chore(release): update version and changelog',
+      }),
     },
     { name: '推送', use: Tasks.Git.push() },
     {
-      name: '发布到npm',
-      skip: isPatchVersion,
-      use: Tasks.Shell.run({ cmd: 'npm publish' }),
+      name: '推送tag',
+      use: Tasks.Shell.run({ cmd: 'git push --tags' }),
     },
     {
-      name: '推送tag',
-      skip: isPatchVersion,
-      use: Tasks.Shell.run({ cmd: 'git push --tags' }),
+      name: '发布到npm',
+      skip: async () =>
+        !(await Tasks.AskFor.shouldContinue({ message: '是否更新至npm？' })()),
+      use: Tasks.Shell.run({ cmd: 'npm publish' }),
     },
   ],
 });
 
-new Workflow('build-and-link', {
-  description: '构建并本地测试',
+new Workflow('release:minor', {
+  description: 'minor版本',
   steps: [
-    { name: '构建', use: Tasks.Shell.run({ cmd: 'npm run build' }) },
-    { name: '移除软链', use: Tasks.Shell.run({ cmd: 'npm unlink' }) },
-    { name: '本地软链', use: Tasks.Shell.run({ cmd: 'npm link' }) },
+    {
+      name: '构建',
+      skip: async () =>
+        !(await Tasks.AskFor.shouldContinue({ message: '是否需要构建？' })()),
+      use: Tasks.Shell.run({ cmd: 'npm run build' }),
+    },
+    { name: '获取提交信息', use: Tasks.AskFor.commitMessage() },
+    { name: '提交', use: Tasks.Git.commit(message => [{ message }]) },
+    {
+      name: '打版本',
+      use: Tasks.Shell.run({
+        cmd: 'npm run version:minor',
+      }),
+    },
+    {
+      name: '生成日志',
+      use: Tasks.Shell.run({
+        cmd: 'npm run changelog',
+      }),
+    },
+    {
+      name: '提交',
+      use: Tasks.Git.commit({
+        message: 'chore(release): update version and changelog',
+      }),
+    },
+    { name: '推送', use: Tasks.Git.push() },
+    {
+      name: '推送tag',
+      use: Tasks.Shell.run({ cmd: 'git push --tags' }),
+    },
+    {
+      name: '发布到npm',
+      skip: async () =>
+        !(await Tasks.AskFor.shouldContinue({ message: '是否更新至npm？' })()),
+      use: Tasks.Shell.run({ cmd: 'npm publish' }),
+    },
+  ],
+});
+
+new Workflow('release:patch', {
+  description: 'patch版本',
+  steps: [
+    {
+      name: '构建',
+      skip: async () =>
+        !(await Tasks.AskFor.shouldContinue({ message: '是否需要构建？' })()),
+      use: Tasks.Shell.run({ cmd: 'npm run build' }),
+    },
+    { name: '获取提交信息', use: Tasks.AskFor.commitMessage() },
+    { name: '提交', use: Tasks.Git.commit(message => [{ message }]) },
+    {
+      name: '打版本',
+      use: Tasks.Shell.run({
+        cmd: 'npm run version:patch',
+      }),
+    },
+    {
+      name: '生成日志',
+      use: Tasks.Shell.run({
+        cmd: 'npm run changelog',
+      }),
+    },
+    {
+      name: '提交',
+      use: Tasks.Git.commit({
+        message: 'chore(release): update version and changelog',
+      }),
+    },
+    { name: '推送', use: Tasks.Git.push() },
+    {
+      name: '推送tag',
+      use: Tasks.Shell.run({ cmd: 'git push --tags' }),
+    },
+    {
+      name: '发布到npm',
+      skip: async () =>
+        !(await Tasks.AskFor.shouldContinue({ message: '是否更新至npm？' })()),
+      use: Tasks.Shell.run({ cmd: 'npm publish' }),
+    },
   ],
 });
